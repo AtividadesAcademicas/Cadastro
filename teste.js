c11ed5608a01750f08b4f456abfde05b1db047da
Pessinaaa <pedro.pessina99@gmail.com>
2022-08-31 11:16:26 -0300

    Aula 01

---
+++ teste.js
@@ -0,0 +1,8 @@
+// npm install express --save
+const express = require('express')
+const app = express()
+
+
+app.listen(8081, function(){
+    console.log('Servidor rodando na url http://localhost:8081')
+})
