94d1685e7225d311542e47aea19dd67fdc3597ba
Pessinaaa <pedro.pessina99@gmail.com>
2022-09-14 10:38:31 -0300

    Aula 02: rotas e nodemon

--- teste.js
+++ teste.js
@@ -1,8 +1,8 @@
-// npm install express --save
 const express = require('express')
 const app = express()
 
+app.get("/", (req, resp) => {resp.send("Bem vindo ao meu app")})
+app.get("/teste1", (req, resp) => {resp.send("Esta é uma rota para uma página")})
+app.get("/teste2", (req, resp) => {resp.send("Esta é uma rota para outra página")})
 
-app.listen(8081, function(){
-    console.log('Servidor rodando na url http://localhost:8081')
-})
+app.listen(8081, () => {console.log('Servidor rodando na url http://localhost:8081')})
